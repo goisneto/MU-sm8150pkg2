db47b075f28caab393939c8a6dcff8903b78c577
sunflower2333 <sunflower2333@outlook.com>
2022-09-24 09:06:42 +0000

    update run.sh again

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-python3 -m venv SurfaceDuo && source SurfaceDuo/bin/activate && python3 ./Platforms/SurfaceDuoPkg/StampBuild.py && ./build_uefi.sh xiaomi-andromeda 6
+#!/bin/bash
+
+python3 -m venv SurfaceDuo
+source SurfaceDuo/bin/activate
+python3 ./Platforms/SurfaceDuoPkg/StampBuild.py
+./build_uefi.sh xiaomi-andromeda 6
