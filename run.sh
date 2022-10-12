4c0b2ccde787fd559516e0fb4cb9e0e10736c4e3
̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳ <3611208+goisneto@users.noreply.github.com>
2022-10-12 13:09:36 -0300

    Update run.sh
    change device Andromeda to Vayu

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python3 -m venv SurfaceDuo
 source SurfaceDuo/bin/activate
 ./setup_uefi.sh
 python3 ./Platforms/SurfaceDuoPkg/StampBuild.py
-./build_uefi.sh xiaomi-andromeda 6
+./build_uefi.sh xiaomi-vayu 8
