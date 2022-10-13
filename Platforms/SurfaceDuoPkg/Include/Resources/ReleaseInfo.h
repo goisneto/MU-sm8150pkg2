98db73c33f1491ee6a8c02cade5573926b5b617a
̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳̳ <3611208+goisneto@users.noreply.github.com>
2022-10-12 21:51:36 -0300

    Update Submodules to Last Release

---
+++ Platforms/SurfaceDuoPkg/Include/Resources/ReleaseInfo.h
@@ -0,0 +1,19 @@
+#ifndef __SMBIOS_RELEASE_INFO_H__
+#define __SMBIOS_RELEASE_INFO_H__
+#ifdef __IMPL_COMMIT_ID__
+#undef __IMPL_COMMIT_ID__
+#endif
+#define __IMPL_COMMIT_ID__ "5012648b"
+#ifdef __RELEASE_DATE__
+#undef __RELEASE_DATE__
+#endif
+#define __RELEASE_DATE__ "10/12/2022"
+#ifdef __BUILD_OWNER__
+#undef __BUILD_OWNER__
+#endif
+#define __BUILD_OWNER__ "root@GN-PC"
+#ifdef __EDK2_RELEASE__
+#undef __EDK2_RELEASE__
+#endif
+#define __EDK2_RELEASE__ "f07c1b9b"
+#endif
